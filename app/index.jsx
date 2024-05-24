c8f985dbe2d044882760cdba98e410af8b63c61a
Archit Gulve <architgulve@gmail.com>
2024-05-24 17:14:46 +0530

    #

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+    return(
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">Sendwise!</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}    
+ 
