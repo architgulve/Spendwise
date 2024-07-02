f1237bf568fed87b66074a57e50031511eb12c91
Atharva Khedekar <atharvakh7@gmail.com>
2024-07-02 20:49:45 +0400

    i changed enter your name to yeeeeeeeeeeet

--- app/(onboarding)/entername.jsx
+++ app/(onboarding)/entername.jsx
@@ -17,7 +17,7 @@ const entername = () => {
         <View className="w-full justify-between items-center min-h-screen">
           <View className="w-full justify-center items-center">
             <View className="w-full justify-start items-center">
-              <Text className="text-white text-3xl mt-32">Enter your Name</Text>
+              <Text className="text-white text-3xl mt-32">YEEEEEEEEEEEEEET INDIA WONN T20 WC</Text>
             </View>
             <View className="w-full justify-start items-center ">
               <FormField
