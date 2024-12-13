418ff1e04e13139e8178a5381c25034e86e88078
AKSHAD KHUNE <akshadkhune@gmail.com>
2024-12-14 00:01:12 +0530

    add screen 2 calender part solved and some minor changes have been made

--- app/(tabs)/badges.jsx
+++ app/(tabs)/badges.jsx
@@ -5,11 +5,48 @@ import React from "react";
 import Card from "../../components/Card";
 import Button from "../../components/Button";
 import Badge from "../../components/Badge";
-
 import { router } from "expo-router";
 import Animated from "react-native-reanimated";
+import { useState, useCallback, useDebugValue } from "react";
+import { getSumOfMonthExpenses, sumofAllCategories } from "../../utils/database";
+import { useFocusEffect } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 const Badges = () => {
+  const [userBudget, setUserBudget] = useState(5000);
+  const [userName, setUserName] = useState("Stranger");
+  const [monthlyexpenses, setExpenses] = useState([]);
+  
+
+  const fetchData = async () => {
+    try {
+      const budget = await AsyncStorage.getItem("userBudget");
+      if (budget !== null) {
+        setUserBudget(budget);
+      }
+      const name = await AsyncStorage.getItem("userName");
+      if (name !== null) {
+        setUserName(name);
+      const monthlyexpenses = await getSumOfMonthExpenses();
+      if(monthlyexpenses !== null){
+      setExpenses(monthlyexpenses);
+      }
+      }
+    } catch (e) {
+      console.log("Error fetching expenses:", e);
+    }
+  };
+
+  
+
+  useFocusEffect(
+      useCallback(() => {
+        fetchData();
+      }, [])
+    );
+
+
   return (
     <SafeAreaView edges={["top"]} className="bg-[#000000] h-full ">
       <StatusBar hidden={false} style="light" />
@@ -26,55 +63,32 @@ const Badges = () => {
                   <Badge />
                 </Button>
               </Animated.View>
+            </View>
+            <View>
+              <Text className="text-white text-2xl font-bold">
+                {userName}
+              </Text>
+              <Text className="text-white text-2xl font-bold">
+                {userBudget}
+              </Text>
+              <Text className="text-white text-2xl font-bold">
+                {monthlyexpenses}
+              </Text>
+            </View>
+            <View className="bg-white h-[50vw] w-full rounded-2xl flex flex-col justify-between">
               <View>
-                <Button>
-                  <Badge />
-                </Button>
+              <Text className="text-black text-2xl font-bold "> Monthly Rewards</Text>
               </View>
-              <View>
-                <Button>
-                  <Badge />
-                </Button>
-              </View>
-              <View>
-                <Button>
-                  <Badge />
-                </Button>
-              </View>
-              <View>
-                <Button>
-                  <Badge />
-                </Button>
-              </View>
-              <View>
-                <Button>
-                  <Badge />
-                </Button>
-              </View>
-              <View>
-                <Button>
-                  <Badge />
-                </Button>
-              </View>
-              <View>
-                <Button>
-                  <Badge />
-                </Button>
-              </View>
-              <View>
-                <Button>
-                  <Badge />
-                </Button>
-              </View>
-              <View>
-                <Button>
-                  <Badge />
-                </Button>
-              </View>
-              <View>
-                <Button>
-                  <Badge />
-                </Button>
+              <View className="flex flex-row space-x-2">
+                {monthlyexpenses<userBudget ?
+                  <Badge/>
+                  :
+                  <View className="flex h-[45vw] w-[45vw] bg-black">
+                    {/* <Badge/> */}
+                    {/* base condition if user doesnt gets a badge */}
+                  </View>
+
+                }
               </View>
             </View>
           </View>
